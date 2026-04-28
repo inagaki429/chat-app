ac4cf9164d71c2b5fc78fa044199f0f57fad9381
inagaki429 <inagaki@ibis-japan.co.jp>
2026-04-28 14:25:04 +0900

    usersテーブル・リポジトリ・エンティティの作成

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+   id       SERIAL             NOT NULL,
+   nickname VARCHAR(128)    NOT NULL, 
+   -- カラム名nicknameからnameに変えました。
+   email VARCHAR(128)    NOT NULL UNIQUE,
+   password VARCHAR(512)    NOT NULL,
+   PRIMARY KEY (id)
+);
